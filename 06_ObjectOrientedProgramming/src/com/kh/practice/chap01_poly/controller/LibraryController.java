b007946ec9d3d130bf3420d49bc2177655135ef7
JM00001 <liser1@naver.com>
2023-06-07 08:54:46 +0900

    .

--- 06_ObjectOrientedProgramming/src/com/kh/practice/chap01_poly/controller/LibraryController.java
+++ 06_ObjectOrientedProgramming/src/com/kh/practice/chap01_poly/controller/LibraryController.java
@@ -1,12 +1,17 @@
 package com.kh.practice.chap01_poly.controller;
 
+import com.kh.practice.chap01_poly.model.vo.AniBook;
 import com.kh.practice.chap01_poly.model.vo.Book;
+import com.kh.practice.chap01_poly.model.vo.CookBook;
 import com.kh.practice.chap01_poly.model.vo.Member;
 
 public class LibraryController {
 	private Member mem = null;
-	private Book[] bList = new Book[5] ;
-	
+	private Book[] bList = new Book[5];
+	{
+		bList[0] = new CookBook("백종원의 집밥", "백종원", "tvN", true);
+		bList[1] = new AniBook("한번 더 해요", "미티", "원모어", 19);
+	};	
 	public void insertMember(Member mem) {
 		this.mem = mem;
 	}
@@ -20,7 +25,7 @@ public class LibraryController {
 	}
 
 	public Book[] searchBook(String keyword) {
-		return book[];
+		return null;
 		
 	}
 	
