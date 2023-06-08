d911720aa0921b1bcde086bb2e43c1eae81c3139
JM00001 <liser1@naver.com>
2023-06-08 16:52:29 +0900

    중첩 반복문 종료하기, 재귀함수

--- 06_ObjectOrientedProgramming/src/com/kh/practice/comp/run/Run.java
+++ 06_ObjectOrientedProgramming/src/com/kh/practice/comp/run/Run.java
@@ -1,7 +1,11 @@
 package com.kh.practice.comp.run;
 
+import com.kh.practice.comp.func.CompExample;
+
 public class Run {
 	public static void main(String[] args) {
+//		new CompExample().practice4();
+		new CompExample().practice4_1();
 		
 	}
 
